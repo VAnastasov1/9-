7ebb90831aebaec44e1827bc5c4629833bc69ff5
VAnastasov1 <157456396+VAnastasov1@users.noreply.github.com>
2024-01-23 09:33:40 +0200

    Add files via upload

---
+++ 18.01.24/18.01.24/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._01._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string listOfBeers = "Amstel, Zagorka, Tuborg, Becks.";
+            string[] beers = listOfBeers.Split(' ', ',', '.');
+            Console.WriteLine("Available beers are:");
+            foreach (string beer in beers)
+                Console.WriteLine(beer);
+
+            string listOfBeers1 = "Az,Valenin,obi4am,da,piq,voda";
+            string[] beers1 = listOfBeers1.Split(' ', ',', '.');
+            Console.WriteLine("Завърших защото:");
+            foreach (string Az in beers1)
+                Console.WriteLine(Az);
+
+            string replaced = listOfBeers.Replace(",", " ");
+            Console.WriteLine(replaced);
+
+            string novo = replaced.Remove(6, 10);
+            Console.WriteLine(novo);
+            string lowerAlpha = novo.ToLower();
+            Console.WriteLine(lowerAlpha);
+            string upperAlpha = listOfBeers1.ToUpper();
+            Console.WriteLine(upperAlpha);
+
+            string novo1 = listOfBeers1.Replace(",", " ");
+
+            string nov = listOfBeers1.Remove(25,4);
+            Console.WriteLine(nov);
+            
+            
+        }
+    }
+}
